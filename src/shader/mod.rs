cb1740f281ff5c4959feb4afc9ef26996cb77db6
隽 康 <kangjun1120@hotmail.com>
2024-11-17 17:20:23 +0800

    for floder

--- src/shader/mod.rs
+++ src/shader/mod.rs
@@ -2,9 +2,9 @@ use glium::backend::glutin::Display;
 use glium::glutin::surface::WindowSurface;
 
 pub fn shader_pro(display: &Display<WindowSurface>) -> glium::program::Program {
-    let vertex_shader_src = std::str::from_utf8(include_bytes!("vertex_shader.glsl"))
+    let vertex_shader_src = std::str::from_utf8(include_bytes!("gl/vertex_shader.glsl"))
         .expect("Failed to read vertex shader");
-    let fragment_shader_src = std::str::from_utf8(include_bytes!("fragment_shader.glsl"))
+    let fragment_shader_src = std::str::from_utf8(include_bytes!("gl/fragment_shader.glsl"))
         .expect("Failed to read fragment shader");
 
     let program =
