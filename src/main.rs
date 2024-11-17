bef1e7226d694a93573d2e5c8463b381e5ae38d6
隽 康 <kangjun1120@hotmail.com>
2024-11-17 17:10:52 +0800

    Upgrade

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate glium;
 use glium::Surface;
+use shader::shader_pro;
+
+mod shader;
 
 fn main() {
     let event_loop = glium::winit::event_loop::EventLoop::builder()
@@ -10,10 +13,15 @@ fn main() {
         .with_title("Glium tutorial #6")
         .build(&event_loop);
 
-    let image = image::load(std::io::Cursor::new(&include_bytes!("./opengl.png")),
-                            image::ImageFormat::Png).unwrap().to_rgba8();
+    let image = image::load(
+        std::io::Cursor::new(&include_bytes!("./opengl.png")),
+        image::ImageFormat::Png,
+    )
+    .unwrap()
+    .to_rgba8();
     let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let image =
+        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
     let texture = glium::Texture2d::new(&display, image).unwrap();
 
     #[derive(Copy, Clone)]
@@ -24,32 +32,44 @@ fn main() {
     implement_vertex!(Vertex, position, tex_coords);
 
     let shape = vec![
-        Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] },
-        Vertex { position: [ 0.5, -0.5], tex_coords: [1.0, 0.0] },
-        Vertex { position: [ 0.5,  0.5], tex_coords: [1.0, 1.0] },
-        Vertex { position: [ 0.5,  0.5], tex_coords: [1.0, 1.0] },
-        Vertex { position: [-0.5,  0.5], tex_coords: [0.0, 1.0] },
-        Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] },
+        Vertex {
+            position: [-0.5, -0.5],
+            tex_coords: [0.0, 0.0],
+        },
+        Vertex {
+            position: [0.5, -0.5],
+            tex_coords: [1.0, 0.0],
+        },
+        Vertex {
+            position: [0.5, 0.5],
+            tex_coords: [1.0, 1.0],
+        },
+        Vertex {
+            position: [0.5, 0.5],
+            tex_coords: [1.0, 1.0],
+        },
+        Vertex {
+            position: [-0.5, 0.5],
+            tex_coords: [0.0, 1.0],
+        },
+        Vertex {
+            position: [-0.5, -0.5],
+            tex_coords: [0.0, 0.0],
+        },
     ];
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
     // 加载着色器文件
-    let vertex_shader_src = std::str::from_utf8(include_bytes!("vertex_shader.glsl"))
-        .expect("Failed to read vertex shader");
-    let fragment_shader_src = std::str::from_utf8(include_bytes!("fragment_shader.glsl"))
-        .expect("Failed to read fragment shader");
-
-    let program = glium::Program::from_source(&display, &vertex_shader_src, &fragment_shader_src, None)
-        .expect("Failed to create program");
+    let program = shader_pro(&display);
 
     #[allow(deprecated)]
-    event_loop.run(move |ev, window_target| {
-        match ev {
+    event_loop
+        .run(move |ev, window_target| match ev {
             glium::winit::event::Event::WindowEvent { event, .. } => match event {
                 glium::winit::event::WindowEvent::CloseRequested => {
                     window_target.exit();
-                },
+                }
                 glium::winit::event::WindowEvent::RedrawRequested => {
                     let mut target = display.draw();
                     target.clear_color(0.0, 0.0, 1.0, 1.0);
@@ -58,20 +78,26 @@ fn main() {
                         tex: &texture,
                     };
 
-                    target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                                &Default::default()).unwrap();
+                    target
+                        .draw(
+                            &vertex_buffer,
+                            &indices,
+                            &program,
+                            &uniforms,
+                            &Default::default(),
+                        )
+                        .unwrap();
                     target.finish().unwrap();
-                },
+                }
                 glium::winit::event::WindowEvent::Resized(window_size) => {
                     display.resize(window_size.into());
-                },
+                }
                 _ => (),
             },
             glium::winit::event::Event::AboutToWait => {
                 window.request_redraw();
-            },
+            }
             _ => (),
-        }
-    })
-    .unwrap();
+        })
+        .unwrap();
 }
---
+++ src/render_loop.rs
@@ -0,0 +1,43 @@
+use glium::{Display, Surface, Program, VertexBuffer, index::NoIndices, texture::Texture2d};
+use glium::index::PrimitiveType;
+
+pub fn run_event_loop(
+    event_loop: glium::winit::event_loop::EventLoop<()>,
+    display: Display,
+    program: Program,
+    vertex_buffer: VertexBuffer<impl glium::vertex::Vertex>,
+    indices: NoIndices,
+    texture: Texture2d,
+) {
+    #[allow(deprecated)]
+    event_loop.run(move |ev, window_target| {
+        match ev {
+            glium::winit::event::Event::WindowEvent { event, .. } => match event {
+                glium::winit::event::WindowEvent::CloseRequested => {
+                    window_target.exit();
+                },
+                glium::winit::event::WindowEvent::RedrawRequested => {
+                    let mut target = display.draw();
+                    target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+                    let uniforms = uniform! {
+                        tex: &texture,
+                    };
+
+                    target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                                &Default::default()).unwrap();
+                    target.finish().unwrap();
+                },
+                glium::winit::event::WindowEvent::Resized(window_size) => {
+                    display.resize(window_size.into());
+                },
+                _ => (),
+            },
+            glium::winit::event::Event::AboutToWait => {
+                window.request_redraw();
+            },
+            _ => (),
+        }
+    })
+    .unwrap();
+}
