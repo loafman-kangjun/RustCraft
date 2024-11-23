d2d6e943de1c89639677f5976aa5a92a1a0ca343
隽 康 <kangjun1120@hotmail.com>
2024-11-23 12:00:24 +0800

    USE SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,44 @@
-#[macro_use]
-extern crate glium;
-use cgmath;
-use glium::Surface;
-use shader::shader_pro;
-use shapevec::CUBE;
+extern crate sdl2;
 
-mod pngconver;
-mod shader;
-mod shapevec;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
 
-fn main() {
-    let mut rotation_angle = 0.0;
-    let event_loop = glium::winit::event_loop::EventLoop::builder()
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
+        .position_centered()
         .build()
-        .expect("event loop building");
-    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
-        .with_title("Glium tutorial #6")
-        .build(&event_loop);
-
-    let image = pngconver::image();
-    let texture = glium::Texture2d::new(&display, image).unwrap();
-
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-    let vertex_buffer = glium::VertexBuffer::new(&display, &CUBE).unwrap();
-
-    let draw_params = glium::DrawParameters {
-        depth: glium::Depth {
-            test: glium::draw_parameters::DepthTest::IfLess,
-            write: true,
-            ..Default::default()
-        },
-        ..Default::default()
-    };
-
-    window.set_cursor_visible(false);
-
-    struct Camera {
-        position: cgmath::Point3<f32>,
-        yaw: f32,   // 水平方向
-        pitch: f32, // 垂直方向
-    }
-
-    impl Camera {
-        fn new(position: cgmath::Point3<f32>, yaw: f32, pitch: f32) -> Self {
-            Camera {
-                position,
-                yaw,
-                pitch,
-            }
-        }
-
-        fn get_view_matrix(&self) -> cgmath::Matrix4<f32> {
-            let forward = cgmath::Vector3 {
-                x: self.yaw.cos() * self.pitch.cos(),
-                y: self.pitch.sin(),
-                z: self.yaw.sin() * self.pitch.cos(),
-            };
-            let target = cgmath::Point3::new(0.0, 0.0, 0.0) + forward;
-            let up = cgmath::Vector3::new(0.0, 1.0, 0.0);
-            println!("{:?}", target);
-            cgmath::Matrix4::look_at_rh(self.position, target, up)
-        }
-    }
-
-    // 加载着色器文件
-    let program = shader_pro(&display);
-
-    let mut camera = Camera::new(cgmath::Point3::new(1.5, 1.5, 1.5), 0.0, 0.0);
-    let mut last_mouse_position = glium::winit::dpi::PhysicalPosition::new(400.0, 300.0);
-
-    #[allow(deprecated)]
-    event_loop
-        .run(move |ev, window_target| match ev {
-            glium::winit::event::Event::WindowEvent { event, .. } => match event {
-                glium::winit::event::WindowEvent::CloseRequested => {
-                    window_target.exit();
-                }
-                glium::winit::event::WindowEvent::CursorMoved { position, .. } => {
-                    let delta_x = position.x as f32 - last_mouse_position.x as f32;
-                    let delta_y = position.y as f32 - last_mouse_position.y as f32;
-
-                    // 调整摄像头方向
-                    let sensitivity = 0.002;
-                    camera.yaw += delta_x * sensitivity;
-                    camera.pitch -= delta_y * sensitivity;
-
-                    // 限制 pitch 范围
-                    camera.pitch = camera.pitch.clamp(
-                        -std::f32::consts::FRAC_PI_2 + 0.1,
-                        std::f32::consts::FRAC_PI_2 - 0.1,
-                    );
-
-                    last_mouse_position = position;
-                }
-                glium::winit::event::WindowEvent::RedrawRequested => {
-                    let mut target = display.draw();
-                    target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
-                    let window_size = window.inner_size();
-                    let aspect_ratio = window_size.width as f32 / window_size.height as f32;
-                    let perspective =
-                        cgmath::perspective(cgmath::Deg(45.0), aspect_ratio, 0.1, 100.0);
-                    let model = cgmath::Matrix4::from_angle_y(cgmath::Deg(rotation_angle));
-                    let view = camera.get_view_matrix();
-                    let uniforms = uniform! {
-                        tex: &texture,
-                        perspective: Into::<[[f32; 4]; 4]>::into(perspective),
-                        view: Into::<[[f32; 4]; 4]>::into(view),
-                        model: Into::<[[f32; 4]; 4]>::into(model),
-                    };
-
-                    target
-                        .draw(&vertex_buffer, &indices, &program, &uniforms, &draw_params)
-                        .unwrap();
-                    target.finish().unwrap();
-                }
-                glium::winit::event::WindowEvent::Resized(window_size) => {
-                    display.resize(window_size.into());
-                    window.request_redraw();
-                }
-                _ => (),
-            },
-            glium::winit::event::Event::AboutToWait => {
-                rotation_angle += 0.01;
-                window.request_redraw();
-            }
-            _ => (),
-        })
         .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
