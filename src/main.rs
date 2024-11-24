6febd9c0015c17022567d9a87bc7581859522d3b
隽 康 <kangjun1120@hotmail.com>
2024-11-24 17:49:26 +0800

    Add page

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod utils;
 use sdl2::event::Event;
 use std::time::{Duration, Instant};
 use utils::gl_utils::{find_sdl_gl_driver, init_opengl, render_opengl_scene};
-use utils::sdl_utils::show_start_screen;
+use utils::sdl_utils::{show_start_screen, show_declaration_page};
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -22,32 +22,41 @@ fn main() {
         .unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    if show_start_screen(&mut canvas, &mut event_pump) {
-        let shader_program = init_opengl(&video_subsystem);
+    // 在初始页面选择
+    match show_start_screen(&mut canvas, &mut event_pump) {
+        Some("opengl") => {
+            let shader_program = init_opengl(&video_subsystem);
 
-        let mut last_time = Instant::now();
-        let mut frame_count = 0;
+            let mut last_time = Instant::now();
+            let mut frame_count = 0;
 
-        'opengl_loop: loop {
-            let current_time = Instant::now();
-            let elapsed = current_time - last_time;
-            frame_count += 1;
+            'opengl_loop: loop {
+                let current_time = Instant::now();
+                let elapsed = current_time - last_time;
+                frame_count += 1;
 
-            if elapsed >= Duration::from_secs(1) {
-                let fps = frame_count as f32 / elapsed.as_secs_f32();
-                println!("FPS: {:.2}", fps); // 仅供调试，最终需要绘制到屏幕上
-                frame_count = 0;
-                last_time = current_time;
-            }
-
-            for event in event_pump.poll_iter() {
-                if let Event::Quit { .. } = event {
-                    break 'opengl_loop;
+                if elapsed >= Duration::from_secs(1) {
+                    let fps = frame_count as f32 / elapsed.as_secs_f32();
+                    println!("FPS: {:.2}", fps);
+                    frame_count = 0;
+                    last_time = current_time;
                 }
-            }
 
-            render_opengl_scene(shader_program);
-            canvas.present();
+                for event in event_pump.poll_iter() {
+                    if let Event::Quit { .. } = event {
+                        break 'opengl_loop;
+                    }
+                }
+
+                render_opengl_scene(shader_program);
+                canvas.present();
+            }
+        }
+        Some("declaration") => {
+            show_declaration_page(&mut canvas, &mut event_pump);
+        }
+        _ => {
+            println!("Exiting...");
         }
     }
 }
